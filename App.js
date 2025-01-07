25328a2b293ef6a3276a1fd9fef6a6a534bbfaf7
xop141 <lowsens400@gmail.com>
2025-01-07 12:07:36 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import React, { useState } from 'react';
+import Moment from 'react-moment';
 
 let id_counter = 0;
+let Test = 0
 
 function App() {
   const [todo, setTodo] = useState([]);
   const [input, setInput] = useState("");
   const [filter, setFilter] = useState("all");
+
+  
+ 
 
 
   const handle_input = (event) => {
@@ -15,48 +20,62 @@ function App() {
 
   const add_task = () => {
     if (input.length === 0) {
-      return; 
+      alert("asd") 
     } else {
       id_counter = id_counter + 1;
       setTodo([...todo, { text: input, status: 'active', id: id_counter }]);
       setInput("");
     }
   };
-
+const showLOG=()=>{
+  
+}
 
 
   const statusdisplayer = (id) => {
+  
+  
+  
+   
+    
+    
     setTodo(todo.map(tod =>
       tod.id === id ? { ...tod, status: tod.status === 'done' ? 'active' : 'done' } : tod
+
     ));
     
     
   };
- // const statuschecker = (tod) => {
-   // return tod.status === 'done';
-  //};
+ 
+ 
   const deleteALL=()=>{
     setTodo([])
   }
-const deleteTask=()=>{
-  console.log(1);
+
+
+  const deleteTask=(id)=>{
+    const updated = todo.filter((todos)=>todos.id !== id);
+ setTodo(updated)
+     } 
+
   
-}
+
   const showdone = () => {
     setFilter("done");
+    
+    
   };
 
   const showactive = () => {
     setFilter("active");
+
   };
 
   const showall = () => {
     setFilter("all");
-   
-   
-
     
   };
+
 
   const filteredTodos = todo.filter(tod => {
     if (filter === "done") {
@@ -76,13 +95,13 @@ const deletetask=(event)=>{
       <div id='top'>
         <div id='header'>To-Do list</div>
         <div id='input_field'>
-          <input onChange={handle_input} value={input} id='input' placeholder='add task' />
-          <button id='add_button' onClick={add_task} disabled={input.length === 0}>Add</button>
+          <input onChange={handle_input} value={input} id='input' placeholder=' Add a new task...' />
+          <div id='add_button' onClick={add_task}  >Add</div>
         </div>
         <div id='button_field'>
-          <button onClick={showall}>All</button>
-          <button onClick={showactive}>Active</button>
-          <button onClick={showdone}>Done</button>
+          <div className='test3' onClick={showall}><p>All</p></div>
+          <div className='test3' id='sda' onClick={showactive}><p>Active</p></div>
+          <div className='test3' onClick={showdone}><p>Completed</p></div>
         </div>
         <div id='content_field'>
           {filteredTodos.length === 0 ? (
@@ -95,20 +114,22 @@ const deletetask=(event)=>{
                   id='check'
                   type="checkbox"
                   checked={tod.status === 'done'}
-                  onChange={() => statusdisplayer(tod.id)}
                   
+                  onChange={() => statusdisplayer(tod.id)}
+         
                 />
                 {tod.text}
                 </div>
-                <button id='delete_button_container' onClick={deleteTask}>delete</button>
+                <div id='delete_button_container' onClick={()=>deleteTask(tod.id)}>Delete</div>
  </div>
             ))
           )}
         </div>
       </div>
-      <div id='bottom'>
+      <div id='bottom'  >
       {todo.length} task
-      <button onClick={deleteALL}>delete all</button>
+      <div id='log_button' onClick={showLOG}> </div>
+      <div id='genocide'  onClick={deleteALL}>delete all</div>
       
       </div>
     </div>
